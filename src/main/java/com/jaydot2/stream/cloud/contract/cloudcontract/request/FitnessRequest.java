ff0314d39ae8884faf0400e792eee7d85938bbe8
jbrayjr2006 <jbrayjr2006@gmail.com>
2018-11-07 19:11:40 -0600

    Added configuration support for dead letter queues in RabbitMQ

--- src/main/java/com/jaydot2/stream/cloud/contract/cloudcontract/request/FitnessRequest.java
+++ src/main/java/com/jaydot2/stream/cloud/contract/cloudcontract/request/FitnessRequest.java
@@ -1,13 +1,16 @@
 package com.jaydot2.stream.cloud.contract.cloudcontract.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 public class FitnessRequest {
 
+    @Value("${fitness.exercise: pushups}")
     @JsonProperty
     private String exerciseName;
 
+    @Value("${fitness.description: description}")
     @JsonProperty
     private String exerciseDescription;
 
