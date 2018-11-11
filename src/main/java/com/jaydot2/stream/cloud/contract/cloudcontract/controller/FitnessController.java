6b2cfd5aa3e21713f336024461e1cedb808ffeb0
jbrayjr2006 <jbrayjr2006@gmail.com>
2018-11-11 17:54:33 -0600

    Added manifest.yml configuration file and update the controller and dependencies

--- src/main/java/com/jaydot2/stream/cloud/contract/cloudcontract/controller/FitnessController.java
+++ src/main/java/com/jaydot2/stream/cloud/contract/cloudcontract/controller/FitnessController.java
@@ -2,11 +2,15 @@ package com.jaydot2.stream.cloud.contract.cloudcontract.controller;
 
 import com.jaydot2.stream.cloud.contract.cloudcontract.request.FitnessRequest;
 import com.jaydot2.stream.cloud.contract.cloudcontract.response.FitnessResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalTime;
+
+@Slf4j
 @RestController
 public class FitnessController {
 
@@ -16,6 +20,8 @@ public class FitnessController {
             consumes = MediaType.APPLICATION_JSON_VALUE)
     public FitnessResponse saveMyExercise(FitnessRequest request) {
         FitnessResponse response = new FitnessResponse();
+        log.debug("response object: " + response.getExerciseTime());
+        response.setExerciseTime(LocalTime.now().toString());
         return response;
     }
 }
